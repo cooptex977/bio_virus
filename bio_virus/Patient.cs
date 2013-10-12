a72f2dbf33711364f9b397a7139b7aac958754ef
Cooper <cooper@stevetex.com>
2013-10-11 22:05:56 -0700

    Initial Commit

---
+++ bio_virus/Patient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bio_virus
+{
+    public class SimplePatient
+    {
+        List<SimpleVirus> viruses;
+        int maxpop;
+        public int getpop()
+        {
+            return viruses.Count;
+        }
+        public void update()
+        {
+            List<SimpleVirus> removelist = new List<SimpleVirus>();
+            foreach (SimpleVirus sv in viruses)
+            {
+                if (sv.clear())
+                    removelist.Add(sv);
+            }
+        }
+        public SimplePatient(List<SimpleVirus> Viruses, int MaxPopulation)
+        {
+            viruses = Viruses;
+            maxpop = MaxPopulation;
+        }
+    }
+}
---
+++ bio_virus/Virus.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bio_virus
+{
+    public class SimpleVirus
+    {
+        float maxBirthProb;
+        float clearProb;
+        public bool clear()
+        {
+            if ((float)(Program.rand.NextDouble()) == clearProb)
+                return true;
+            else
+                return false;
+        }
+        public SimpleVirus reproduce(float density)
+        {
+            if ((float)(Program.rand.NextDouble()) <= maxBirthProb * (1 - density))
+            {
+                return new SimpleVirus(maxBirthProb, clearProb);
+            }
+            else
+                throw new NoChildException();
+        }
+        public SimpleVirus(float birthprob, float clearprob)
+        {
+            maxBirthProb = birthprob;
+            clearProb = clearprob;
+        }
+    }
+    public class NoChildException : Exception
+    {
+
+    }
+}
